6f92aeeb2ac787b2c5ef3ffbd1d97ecdf22b0d61
DerLed <48630545+DerLed@users.noreply.github.com>
2022-03-30 22:21:29 +0300

    f

--- src/main/java/com/example/carshop/HelloApplication.java
+++ src/main/java/com/example/carshop/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.SQLException;
+import java.util.List;
 
 import java.io.IOException;
 
@@ -18,6 +20,22 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        try {
+            // Создаем экземпляр по работе с БД
+            DbHandler dbHandler = DbHandler.getInstance();
+            // Добавляем запись
+            //dbHandler.addProduct(new Product("Музей", 200, "Развлечения"));
+            // Получаем все записи и выводим их на консоль
+            List<Product> products = dbHandler.getAllProducts();
+            for (Product product : products) {
+                System.out.println(product.toString());
+            }
+            // Удаление записи с id = 8
+            //dbHandler.deleteProduct(8);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.carshop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens com.example.carshop to javafx.fxml;
